16216a330cd8ea53a00e4f7e9dd70cd4d4c2a50b
Bill Cauchois <wcauchois@gmail.com>
2012-01-08 12:01:35 -0800

    removed database router (was it necessary?)

--- privy/settings.py
+++ privy/settings.py
@@ -24,7 +24,7 @@ DATABASES = {
     }
 }
 
-DATABASE_ROUTERS = ['privy.router.dashboard_router']
+DATABASE_ROUTERS = []
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
