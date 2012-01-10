a08ec1d11e74b3e5c0f6fadecac5f1b08e29f237
Harmanas Chopra <gursikh@gmail.com>
2012-01-09 17:23:02 -0800

    modified:   privy/main/templates/index.html     modified:   privy/settings.py

--- privy/settings.py
+++ privy/settings.py
@@ -62,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'static/' #TODO: CHANGE THIS TO WEBSERVER STATIC URL!!!!!
+STATIC_ROOT = '%s/static/' % os.getcwd() #TODO: CHANGE THIS TO WEBSERVER STATIC URL!!!!!
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
